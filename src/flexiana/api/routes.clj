7821f0f3577a85b76e067248fa8d76f8b91c6f00
Haruo Adachi <rogerio.adachi@paygo.com.br>
2021-05-16 22:09:02 -0300

    Smaller changes

--- src/flexiana/api/routes.clj
+++ src/flexiana/api/routes.clj
@@ -3,41 +3,42 @@
       [io.pedestal.http.route :as route]
       [flexiana.backend.scramble :as service]))
 
-(defn ok [body]
+(defn http-200-response [body]
       {:status 200 :body body})
 
-(defn bad-request [message]
+(defn http-502-response [message]
       {:status 502 :body message})
 
-(defn validate-request [{:keys [pieces puzzle]}]
+(defn validate-request-params [{{:keys [pieces puzzle]}     :query-params}]
+      (-> (or (empty? pieces) (empty? puzzle))
+          not))
+
+(defn bad-request [{{:keys [pieces puzzle]}     :query-params}]
       (cond
-        (and (empty? pieces) (empty? puzzle)) (bad-request (str "pieces and puzzle cannot by empty\n"))
-        (empty? pieces) (bad-request (str "pieces cannot by empty\n"))
-        :else (bad-request (str "puzzle cannot by empty\n"))))
+        (and (empty? pieces) (empty? puzzle)) (http-502-response (str "pieces and puzzle cannot by empty\n"))
+        (empty? pieces) (http-502-response (str "pieces cannot by empty\n"))
+        :else (http-502-response (str "puzzle cannot by empty\n"))))
 
 (defn process-request [pieces puzzle]
-      (cond
-        (or (empty? pieces) (empty? puzzle)) nil
-        (service/scramble? pieces puzzle) "Its possible"
-        :else "Not Possible"))
+      (if (service/scramble? pieces puzzle)
+        "Possible"
+        "Not Possible"))
 
-(defn respond-hello [request]
-      (let [pieces (get-in request [:query-params :pieces])
-            puzzle (get-in request [:query-params :puzzle])
-            input {:pieces pieces :puzzle puzzle}
-            resp (process-request pieces puzzle)]
-           (if resp
-             (ok resp)
-             (validate-request input))))
+(defn scramble-challenge [{{:keys [pieces puzzle]}     :query-params
+                            :as                         request}]
+      (if (validate-request-params request)
+        (http-200-response (process-request pieces puzzle))
+        (bad-request request)))
 
 (def routes
   (route/expand-routes
-    #{["/greet" :get respond-hello :route-name :greet]}))
+    #{["/greet" :get scramble-challenge :route-name :greet]}))
 
 (def service-map
   {::http/routes routes
    ::http/type   :jetty
-   ::http/port   8890})
+   ::http/port   8890
+   ::http/allowed-origins ["http://localhost:3449"]})
 
 (defn start []
       (http/start (http/create-server service-map)))
