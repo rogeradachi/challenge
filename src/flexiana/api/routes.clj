7e00625a537324ca3e0ce29fed126c65a8081700
Haruo Adachi <rogerio.adachi@paygo.com.br>
2021-05-16 22:31:56 -0300

    A litthe bit of tweak

--- src/flexiana/api/routes.clj
+++ src/flexiana/api/routes.clj
@@ -3,21 +3,21 @@
       [io.pedestal.http.route :as route]
       [flexiana.backend.scramble :as service]))
 
-(defn http-200-response [body]
-      {:status 200 :body body})
+(def ok 200)
+(def bad-request 502)
 
-(defn http-502-response [message]
-      {:status 502 :body message})
+(defn http-response [status-code body]
+      {:status status-code :body body})
 
 (defn validate-request-params [{{:keys [pieces puzzle]}     :query-params}]
       (-> (or (empty? pieces) (empty? puzzle))
           not))
 
-(defn bad-request [{{:keys [pieces puzzle]}     :query-params}]
+(defn bad-request-handler [{{:keys [pieces puzzle]}     :query-params}]
       (cond
-        (and (empty? pieces) (empty? puzzle)) (http-502-response (str "pieces and puzzle cannot by empty\n"))
-        (empty? pieces) (http-502-response (str "pieces cannot by empty\n"))
-        :else (http-502-response (str "puzzle cannot by empty\n"))))
+        (and (empty? pieces) (empty? puzzle)) (http-response bad-request (str "pieces and puzzle cannot by empty\n"))
+        (empty? pieces) (http-response bad-request  (str "pieces cannot by empty\n"))
+        :else (http-response bad-request  (str "puzzle cannot by empty\n"))))
 
 (defn process-request [pieces puzzle]
       (if (service/scramble? pieces puzzle)
@@ -27,8 +27,8 @@
 (defn scramble-challenge [{{:keys [pieces puzzle]}     :query-params
                             :as                         request}]
       (if (validate-request-params request)
-        (http-200-response (process-request pieces puzzle))
-        (bad-request request)))
+        (http-response ok (process-request pieces puzzle))
+        (bad-request-handler request)))
 
 (def routes
   (route/expand-routes
